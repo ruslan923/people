a943ac3e891e1aecbcde39321053ab5fe6fd482c
ruslan923 <ruslan.led@yandex.ru>
2021-05-22 12:27:39 +0300

    Second commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,15 +1,24 @@
 package ru.netology
 
 fun main() {
-    val likes = 56
-    val likestToString = likes.toString()
-    if (likes == 0) {
-        print("Пока никому не понравилось")
-    } else {
-        if (likestToString.endsWith("1")) {
-            print("Понравилось $likes человеку")
-        } else {
-            print("Понравилось $likes людям")
+    val likes = readLine()
+    val remainderOfDivision = likes!!.toInt() % 100
+    val result: String = when {
+        likes.toInt() == 0 -> {
+            "Пока никому не понравилось"
+        }
+        likes.toInt() == 1 -> {
+            "Понравилось $likes человеку"
+        }
+        remainderOfDivision == 1 -> {
+            "Понравилось $likes человеку"
+        }
+        remainderOfDivision in 2..9 -> {
+            "Понравилось $likes человекам"
+        }
+        else -> {
+            "Понравилось $likes людям"
         }
     }
+    print(result)
 }
